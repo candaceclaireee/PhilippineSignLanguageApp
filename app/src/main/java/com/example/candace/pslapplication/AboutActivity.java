dd28508d8da6ee7d7225966b9befadd1d135fba1
candaceclaireee <mercadocandaceclaire@gmail.com>
2018-11-17 00:12:41 +0800

    Added Missing Files

---
+++ app/src/main/java/com/example/candace/pslapplication/AboutActivity.java
@@ -0,0 +1,89 @@
+package com.example.candace.pslapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class AboutActivity extends AppCompatActivity {
+
+    /* For the Navigation Menu */
+    protected DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawer;
+    private NavigationView navView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_about);
+
+        initializeNavigationMenu();
+    }
+
+    /* For Navigation Menu */
+    public void initializeNavigationMenu(){
+        drawerLayout = (DrawerLayout)findViewById(R.id.activity_main);
+        actionBarDrawer = new ActionBarDrawerToggle(this, drawerLayout,R.string.open, R.string.close);
+
+        drawerLayout.addDrawerListener(actionBarDrawer);
+        actionBarDrawer.syncState();
+
+        ActionBar myActionBar = getSupportActionBar();
+        if (myActionBar != null) {
+            myActionBar.setDisplayHomeAsUpEnabled(true);
+            myActionBar.setTitle("About");
+        }
+
+        navView = (NavigationView) findViewById(R.id.nv);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch(id) {
+                    case R.id.home:
+                        drawerLayout.closeDrawers();
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        AboutActivity.this.startActivity(intent);
+                        return true;
+                    case R.id.favorites:
+                        Toast.makeText(AboutActivity.this, "Favorites",Toast.LENGTH_SHORT).show();  break;
+                    case R.id.dictionary:
+                        Toast.makeText(AboutActivity.this, "Dictionary",Toast.LENGTH_SHORT).show();  break;
+                    case R.id.tutorials:
+                        Toast.makeText(AboutActivity.this, "Tutorials",Toast.LENGTH_SHORT).show();  break;
+                    case R.id.categories:
+                        Toast.makeText(AboutActivity.this, "Categories",Toast.LENGTH_SHORT).show();  break;
+                    case R.id.quizzes:
+                        Toast.makeText(AboutActivity.this, "Mini Games",Toast.LENGTH_SHORT).show(); break;
+                    case R.id.about:
+                        return true;
+                    default:
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    /* For Navigation Menu */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(actionBarDrawer.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
+}
