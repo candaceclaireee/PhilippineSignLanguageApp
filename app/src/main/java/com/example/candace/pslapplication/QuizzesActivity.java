0a1e9801fb80c486cdf3fd5c94ea0240c78c5d9a
Jaymee Villacruz <jaymee143@users.noreply.github.com>
2018-12-05 00:38:19 +0800

    Added Quiz part but not functional

--- app/src/main/java/com/example/candace/pslapplication/QuizzesActivity.java
+++ app/src/main/java/com/example/candace/pslapplication/QuizzesActivity.java
@@ -1,6 +1,14 @@
 package com.example.candace.pslapplication;
 
+import android.app.AlarmManager;
+import android.app.AlertDialog;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.Image;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -9,8 +17,13 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class QuizzesActivity extends AppCompatActivity {
 
@@ -18,6 +31,12 @@ public class QuizzesActivity extends AppCompatActivity {
     protected DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawer;
     private NavigationView navView;
+    private int jobID=0;
+    private static final int TIME = 2000;
+    private boolean stop = false;
+    public static final String UI_UPDATE_TAG = "Project.ActivityReceiver";
+   // private BroadcastReceiver receiver = new ActivityReceiver();
+    private ImageView banner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +44,58 @@ public class QuizzesActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_quizzes);
 
+        /*
+        banner = findViewById(R.id.banner);
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(UI_UPDATE_TAG);
+        registerReceiver(receiver, filter);
+        triggerAlarm();
+        */
         initializeNavigationMenu();
     }
 
+    /* For Timer */
+  /*  public void triggerAlarm(){
+
+        Intent alarmIntent = new Intent(UI_UPDATE_TAG);
+        alarmIntent.putExtra("ID", jobID);
+        jobID++;
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this,1000000+jobID, alarmIntent, 0);
+
+        AlarmManager manager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        manager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + TIME, pendingIntent);
+    }
+
+    public void stopAlarm(View view){
+        stop = true;
+    }
+
+    public void makeService(){
+        Intent alarmIntent = new Intent(UI_UPDATE_TAG);
+        alarmIntent.putExtra("ID", jobID);
+        jobID++;
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1000000+jobID, alarmIntent, 0);
+
+        AlarmManager manager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        manager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + TIME, pendingIntent);
+    }
+
+    public class ActivityReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            *//*try {
+
+            }
+            catch(InterruptedException e) {
+                e.printStackTrace();
+            }*//*
+        }
+    }*/
+
+    /*                                             */
     /* For Navigation Menu */
     public void initializeNavigationMenu(){
         drawerLayout = (DrawerLayout)findViewById(R.id.activity_quizzes);
@@ -35,6 +103,8 @@ public class QuizzesActivity extends AppCompatActivity {
 
         drawerLayout.addDrawerListener(actionBarDrawer);
         actionBarDrawer.syncState();
+
+        quizDialog();
 
         ActionBar myActionBar = getSupportActionBar();
         if (myActionBar != null) {
@@ -111,6 +181,19 @@ public class QuizzesActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+
+    private void quizDialog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        View v = inflater.inflate(R.layout.activity_quiz_dialog, null);
+        builder.setView(v);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
     /* For Navigation Menu */
