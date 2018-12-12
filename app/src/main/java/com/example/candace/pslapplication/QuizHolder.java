eb15ce9fd22ad4047b386bc59fa84f6e6734e983
Candace Claire Mercado <mercadocandaceclaire@gmail.com>
2018-12-12 14:59:44 +0800

    Changed Quizzes Layout

--- app/src/main/java/com/example/candace/pslapplication/QuizHolder.java
+++ app/src/main/java/com/example/candace/pslapplication/QuizHolder.java
@@ -1,17 +1,21 @@
 package com.example.candace.pslapplication;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class QuizHolder extends RecyclerView.ViewHolder {
 
     private Button img;
+    private TextView leveltext;
     private String level;
     private QuizzesActivity activity;
     private ArrayList<LevelModel> list;
@@ -22,16 +26,12 @@ public class QuizHolder extends RecyclerView.ViewHolder {
         this.activity = activity;
 
         img = itemView.findViewById(R.id.imgBtn);
+        leveltext = itemView.findViewById(R.id.leveltext);
+
         img.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("DEBUGGING", String.valueOf(level));
-//                if(level=="1")
-//                    img.setText("1");
-//                else if(level=="2")
-//                    img.setText("2");
-//                else if(level=="3")
-//                    img.setText("3");
 
                 Intent intent = new Intent(activity.getApplicationContext(), LevelActivity.class);
                 intent.putExtra("LEVEL", level);
@@ -44,10 +44,22 @@ public class QuizHolder extends RecyclerView.ViewHolder {
     }
     public void setLevel(String lvl) {
         this.level = lvl;
-//        img.setText(lvl);
+
+        if (level.equals("1")) {
+            leveltext.setTextColor(ContextCompat.getColor(activity.getApplicationContext(), R.color.Yellow));
+            img.setTextColor(ContextCompat.getColor(activity.getApplicationContext(), R.color.Yellow));
+        } else if (level.equals("2")) {
+            leveltext.setTextColor(ContextCompat.getColor(activity.getApplicationContext(), R.color.Red));
+            img.setTextColor(ContextCompat.getColor(activity.getApplicationContext(), R.color.Red));
+        } else if (level.equals("3")) {
+            leveltext.setTextColor(ContextCompat.getColor(activity.getApplicationContext(), R.color.Blue));
+            img.setTextColor(ContextCompat.getColor(activity.getApplicationContext(), R.color.Blue));
+        }
+
+        leveltext.setText("LEVEL" + lvl);
     }
     public void setImgBtn(int newImg){
-        img.setBackgroundResource(newImg);
+//        img.setBackgroundResource(newImg);
     }
 
     public void setList(ArrayList<LevelModel> lst){ this.list = lst; }
